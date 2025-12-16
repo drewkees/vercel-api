f530bf1e6c2887708e27e4efa59cf51d623f32ad
Andrew C. Baldonado <acbaldonado@bounty.local>
2025-12-16 08:29:26 +0800

    fix link for api

--- api/salesterritory.js
+++ api/salesterritory.js
@@ -1,16 +1,16 @@
 import { getColumn } from "../lib/sheets.js";
 
 export default async function handler(req, res) {
-  console.log("API /company invoked");
+  console.log("API /SALES TERRITORY invoked");
 
   try {
     console.log("Calling getColumn...");
-    const data = await getColumn("Company!A:A");
+    const data = await getColumn("SALES TERRITORY!A:A");
     // console.log("Data received:", data);
 
-    res.status(200).json({ sheet: "Company", columnA: data });
+    res.status(200).json({ sheet: "SALES TERRITORY", columnA: data });
   } catch (err) {
-    console.error("ERROR in /api/company:", err);
+    console.error("ERROR in /api/SALES TERRITORY:", err);
     res.status(500).json({ error: "Failed to read sheet" });
   }
 }
